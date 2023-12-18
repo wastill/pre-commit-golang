1957f2617a88aeacf3c31cd7e4ba41c913144208
wastill <2225144254@qq.com>
2023-12-18 16:10:19 +0800

    feat: repaire unit-tests

--- golangci-lint.sh
+++ golangci-lint.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-set -eu -o pipefail
+fail() {
+  echo "unit tests failed"
+  exit 1
+}
 
-if ! command -v golangci-lint &> /dev/null ; then
-    echo "golangci-lint not installed or available in the PATH" >&2
-    echo "please check https://github.com/golangci/golangci-lint" >&2
-    exit 1
-fi
 
-exec golangci-lint run "$@"
+go test ./... || fail
