1ae91bdf44f4d2ff04ff7a3a8945b4d2e682ca3b
wastill <2225144254@qq.com>
2023-12-18 16:02:37 +0800

    feat: repaire unit-tests

--- go-unit-tests.sh
+++ go-unit-tests.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
-
-fail() {
-  echo "unit tests failed"
-  exit 1
-}
-
-FILES=$(go list ./... | grep -v /vendor/) || fail
-go test -tags=unit -timeout 30s -short -v ${FILES} || fail
+cmd=(go test)
+. "$(dirname "${0}")/lib/cmd-repo-pkg.bash"
